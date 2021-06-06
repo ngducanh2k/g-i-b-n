2c5bed9f7c2ca7068193d95554c12b14845ec39a
ngducanh2k <85271937+ngducanh2k@users.noreply.github.com>
2021-06-06 14:48:12 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'CHÀO BẠN NHÉ!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao bạn cứ đi nhầm đường
+    Lạc vào bạn bè mình
+    Đằng sau bạn bè là bạn`,
+    btnIntro: 'Nhấn vào đây để xem tiếp nhé',
+    title: 'Phải chăng bạn đã thấy mình từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng bạn đã say mình từ lúc lướt facebook ',
+    btnYes: 'kết bạn với mình <33',
+    btnNo: 'Không, kết bạn với mình :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao bạn lại gửi cho mình lời kết bạn <3',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'cảm ơn bạn nhé <33333333',
+    mess: 'bạn xinh gái lắm 🥰. xinh gái nhiều nhé 😘😘',
+    messDesc: 'cảm ơn bạn đã gửi lời kết bận cho mình.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100030327638673' //link mess của các bạn. VD: https://www.facebook.com/profile.php?id=100030327638673
+}
